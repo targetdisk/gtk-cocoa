82632e79fb3a3391a8c951e22907ec2d074d74e9
pcostabel <>
2004-11-08 02:04:16 +0000

    Implemented gdk_window_get_size, gdk_window_get_parent

--- Gtk+-Cocoa/cocoa/gdkwindow.m
+++ Gtk+-Cocoa/cocoa/gdkwindow.m
@@ -44,10 +44,82 @@ gdk_window_get_pointer (GdkWindow       *window,
 	NSPoint p;
 
 	p = [window mouseLocationOutsideOfEventStream];
-	*x = p.x;
-	*y = p.y;
+    if(x)
+        *x = p.x;
+    
+    if(y)
+        *y = p.y;
 
 	return window;
 }
 
 
+void
+gdk_window_get_size (GdkWindow *window,
+		     gint       *width,
+		     gint       *height)
+{
+#if 0
+  GdkWindowPrivate *window_private;
+  
+  g_return_if_fail (window != NULL);
+  
+  window_private = (GdkWindowPrivate*) window;
+  
+  if (width)
+    *width = window_private->width;
+  if (height)
+    *height = window_private->height;
+#endif
+if (width)
+    *width = [window frame].size.width;
+  if (height)
+    *height = [window frame].size.height;
+}
+
+GdkWindow*
+gdk_window_get_parent (GdkWindow *window)
+{
+
+  g_return_val_if_fail (window != NULL, NULL);
+  
+  if(![window superview])
+        return [window window];
+        
+  return [window superview];
+
+}
+
+GList*
+gdk_window_get_children (GdkWindow *window)
+{
+/*
+  GdkWindowPrivate *private;
+  GdkWindow *child;
+  GList *children;
+  unsigned int nchildren;
+  unsigned int i;
+  
+  g_return_val_if_fail (window != NULL, NULL);
+  
+  private = (GdkWindowPrivate*) window;
+  if (private->destroyed)
+    return NULL;
+  
+  children = NULL;
+  
+  if (nchildren > 0)
+    {
+      for (i = 0; i < nchildren; i++)
+	{
+	  child = gdk_window_lookup (xchildren[i]);
+          if (child)
+            children = g_list_prepend (children, child);
+	}
+      
+    }
+  
+  return children;
+*/
+return NULL;
+}
